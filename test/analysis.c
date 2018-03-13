1ce00f571e358434de4bf53012c90f1857d3b086
johann <johannmassyn@gmail.com>
2018-03-13 21:08:49 +1300

    Created CMakeList.txt Created testcase.

---
+++ test/analysis.c
@@ -0,0 +1,13 @@
+/* General Purpose Memory Allocator (gpmalloc)
+ * analysis.c
+ *
+ * Copyright (C) 2018
+ * All Rights Reserved
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char ** argv)
+{
+	return EXIT_SUCCESS;
+}
